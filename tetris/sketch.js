8af385fe256f1476923f61dcd2ff5859f379c1e1
Kevin Phan <kblphan@gmail.com>
2019-04-27 17:35:40 -0500

    add o block

--- tetris/sketch.js
+++ tetris/sketch.js
@@ -2,7 +2,12 @@
 let score = 0;
 let playfield = [...Array(10)].map(_ => [...Array(23).fill(0)]);
 let counter = 0;
+let stepLength = 15;
 let isDroping = false;
+let blockRow = 0;
+let blockCol = 5;
+let shape = [];
+
 
 // appearance 
 let block = 25;
@@ -25,6 +30,7 @@ function draw() {
   rect(xOffset, yOffset + 24 * block, 12 * block, 1 * block);
   rect(xOffset + 11 * block, yOffset + 4 * block, 1 * block, 21 * block);
 
+  // current blocks on playfield
   for (let i = 0; i < playfield.length; i++) {
     for (let j = 0; j < playfield[i].length; j++) {
       const element = playfield[i][j];
@@ -34,22 +40,80 @@ function draw() {
            block,
            block);
     }
-    step();
   }
+
+  // block that is dropping
+  if (isDroping) {
+    fill('yellow');
+    for (let i = 0; i < shape.length; i++) {
+      for (let j = 0; j < shape[i].length; j++) {
+        const element = shape[i][j];
+        fill(colors[element]);
+        rect(xOffset + (1 + blockCol + i) * block,
+              yOffset + (1 + blockRow + j) * block,
+              block,
+              block);
+      }
+    }
+  }
+  step();
 }
 
 function step() {
   counter++;
-  if (counter > 60) {
+  if (counter > stepLength) {
     counter = 0;
     advance();
   }
 }
 
 function advance() {
-  
+  if (!isDroping) {
+    spawn();
+    isDroping = true;
+  }
+  if (blockRow + 1 > 22 || playfield[blockCol][blockRow + 1] > 0) {
+    playfield[blockCol][blockRow] = 3;
+    console.log(playfield.toString());
+    console.log("next block value: " + playfield[blockCol][blockRow]);
+    isDroping = false;
+  }
+  else {
+    blockRow++;
+  }
 }
 
 function spawn() {
+  blockRow = 0;
+  blockCol = 5;
+  shape = [[2, 2],
+           [2, 2]];
+}
 
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
+    moveLeft();
+  }
+  if (keyCode === RIGHT_ARROW) {
+    moveRight();
+  }
+  if (keyCode == DOWN_ARROW) {
+    softDrop();
+  }
+}
+
+function moveLeft() {
+  if (blockCol > 0) {
+    blockCol--;
+  }
+}
+
+function moveRight() {
+  if (blockCol < 9) {
+    blockCol++;
+  }
+}
+
+function softDrop() {
+  advance();
 }
